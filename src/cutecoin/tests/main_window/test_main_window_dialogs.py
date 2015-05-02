52fd20aae80a92aecafa596e4d8aaa990229b4dc
Vincent Texier <vit@free.fr>
2015-05-02 17:13:55 +0200

    Problem in unit tests : impossible to do multiple tests with Qtimer in a raw
    First test with a Qtimer is ok, next one is stuck in event loop.
    Closing the widget does not work anymore...

--- src/cutecoin/tests/main_window/test_main_window_dialogs.py
+++ src/cutecoin/tests/main_window/test_main_window_dialogs.py
@@ -19,17 +19,28 @@ class MainWindowDialogsTest(unittest.TestCase):
         QLocale.setDefault(QLocale("en_GB"))
         self.application = Application(sys.argv)
         self.main_window = MainWindow(self.application)
-
-    def test_action_about(self):
-        # select menu
-        self.main_window.actionAbout.trigger()
+        print()
         widgets = qapplication.topLevelWidgets()
         for widget in widgets:
-            if isinstance(widget, PyQt5.QtWidgets.QDialog):
-                self.assertEqual(widget.objectName(), 'AboutPopup')
-                self.assertEqual(widget.isVisible(), True)
-                widget.close()
-                break
+            print(widget.objectName())
+
+    def tearDown(self):
+        print('teardown')
+        self.main_window.close()
+        self.main_window.destroy(True, True)
+        del self.main_window
+        qapplication.closeAllWindows()
+
+    # def test_action_about(self):
+    #     # select menu
+    #     self.main_window.actionAbout.trigger()
+    #     widgets = qapplication.topLevelWidgets()
+    #     for widget in widgets:
+    #         if isinstance(widget, PyQt5.QtWidgets.QDialog):
+    #             self.assertEqual(widget.objectName(), 'AboutPopup')
+    #             self.assertEqual(widget.isVisible(), True)
+    #             widget.close()
+    #             break
 
     def test_action_add_account(self):
         # asynchronous test, cause dialog is waiting user response
@@ -38,14 +49,17 @@ class MainWindowDialogsTest(unittest.TestCase):
         self.main_window.action_add_account.trigger()
 
     def _async_test_action_add_account(self):
+        print('timer add')
         widgets = qapplication.topLevelWidgets()
         for widget in widgets:
-            if isinstance(widget, PyQt5.QtWidgets.QDialog):
-                self.assertEqual(widget.objectName(), 'AccountConfigurationDialog')
+            if isinstance(widget, PyQt5.QtWidgets.QDialog) and widget.objectName() == 'AccountConfigurationDialog':
+                print(widget.objectName())
                 self.assertEqual(widget.isVisible(), True)
+                widget.disconnect()
                 widget.close()
+                widget.destroy()
                 break
-    #
+
     # # fixme: require a app.current_account fixture
     # # def test_action_configure_account(self):
     # #     # asynchronous test, cause dialog is waiting user response
@@ -62,17 +76,35 @@ class MainWindowDialogsTest(unittest.TestCase):
     # #             widget.close()
     # #             break
     #
+    # def test_action_export_account(self):
+    #     # select menu
+    #     self.main_window.action_export.trigger()
+    #
+    #     widgets = qapplication.topLevelWidgets()
+    #     for widget in widgets:
+    #         if isinstance(widget, PyQt5.QtWidgets.QFileDialog):
+    #             self.assertEqual(widget.objectName(), 'ExportFileDialog')
+    #             self.assertTrue(widget.isVisible())
+    #             widget.close()
+    #             break
 
-    def test_action_export_account(self):
+    # fixme: i am tstuck here... impossible to close dialog if another test was done before...???!!!
+    def test_action_import_account(self):
+        # asynchronous test, cause dialog is waiting user response
+        QTimer.singleShot(0, self._async_test_action_import_account)
         # select menu
-        self.main_window.action_export.trigger()
+        self.main_window.action_import.trigger()
 
+    def _async_test_action_import_account(self):
+        print('timer')
         widgets = qapplication.topLevelWidgets()
         for widget in widgets:
-            if isinstance(widget, PyQt5.QtWidgets.QFileDialog):
-                self.assertEqual(widget.objectName(), 'ExportFileDialog')
-                self.assertTrue(widget.isVisible())
+            if isinstance(widget, PyQt5.QtWidgets.QDialog) and widget.objectName() == 'ImportAccountDialog':
+                print(widget.objectName())
+                self.assertEqual(widget.isVisible(), True)
+                widget.disconnect()
                 widget.close()
+                widget.destroy()
                 break
 
 if __name__ == '__main__':
